8f53a36ff462c2f57732a633da66d5ddd74d2373
unknown <jmcure2@gmail.com>
2023-03-15 07:37:30 -0500

    conexion

---
+++ index.js
@@ -0,0 +1,153 @@
+
+
+
+
+
+
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Cethereum%2Clitecoin%2Ccardano%2Cdogecoin&vs_currencies=usd&include_24hr_change=true')
+    .then(res => res.json())
+    .then(json => {
+        const container = document.querySelector('.container');
+        const coins = Object.getOwnPropertyNames(json);
+
+        for (let coin of coins) {
+            const coinInfo = json[`${coin}`];
+            const price = coinInfo.usd;
+            const change = coinInfo.usd_24h_change.toFixed(5);
+
+            container.innerHTML += `
+                <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+                    <div class="coin-logo">
+                        <img src="images/${coin}.png">
+                    </div>
+                    <div class="coin-name">
+                        <h3>${coin}</h3>
+                        <span>/USD</span>
+                    </div>
+                    <div class="coin-price">
+                        <span class="price">$${price}</span>
+                        <span class="change">${change}</span>
+                    </div>
+                </div>
+        `;
+        }
+    });
+
+
+
+    particlesJS(
+
+      {
+        "particles": {
+          "number": {
+            "value": 70,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#07ff16"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 3
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 2,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 136.74429248968178,
+            "color": "#88f58f",
+            "opacity": 1,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 105.57003759917487,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }
+      
+      );
+    
+
